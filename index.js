22e359871620dde061d506b778fbab0b7378957a
jatinch58 <jatinch58@gmail.com>
2022-07-13 15:49:47 +0530

    remove acess key

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes/index");
 const app = express();
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("Database connected sucessfully");
 });
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
